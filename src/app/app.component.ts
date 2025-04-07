ad9afe8ec41ca0ad4bacef1b2c4162fd78c4ebe6
Xiaolin <xiaolin.song@benerail.com>
2025-04-07 09:19:54 +0200

    logout after 1 minutes inactive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {AuthService} from './auth.service';
+import {FormsModule} from '@angular/forms';
+import {InactivityTimerComponent} from './inactivity-timer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, InactivityTimerComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'sesstion-timeout';
+  constructor(private authService: AuthService) { }
+
+  @HostListener('document:keyup', ['$event'])
+  @HostListener('document:click', ['$event'])
+  @HostListener('document:wheel', ['$event'])
+  resetTimer() {
+    this.authService.notifyUserAction();
+  }
 }
